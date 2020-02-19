facfc5ba73df09b0b5dcc48a7e3f6fec79f72bcd
AndreiRoibu <aroibu1@gmail.com>
2020-02-19 18:23:36 +0000

    constructed a function for gif creation

--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 import os
 from sklearn.utils import shuffle
+import os
+import imageio
+import glob
+from natsort import natsorted
+
 
 def load_MNIST():
     
@@ -15,8 +20,35 @@ def load_MNIST():
     X, Y = shuffle(X, Y) # We shuffle the data to break any correlations
 
     return X, Y
+
+def make_gif(hook):
+
+    anim_file = hook+'_dcgan.gif'
+    with imageio.get_writer(anim_file, mode='I') as writer:
+        filenames = glob.glob('./samples/'+hook+'_uniform_samples*.png')
+        filenames = natsorted(filenames)
+        # last = -1
+        for i,filename in enumerate(filenames):
+            # frame = 2*(i**0.5)
+            # print(frame)
+            # if round(frame) > round(last):
+            #     last = frame
+            #     print("---")
+            #     print(last)
+            #     print(">>>>")
+            # else:
+            #     continue
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+
+    # import IPython
+    #     if IPython.version_info > (6,2,0,''):
+    #     display.Image(filename=anim_file)
+
     
 
 # if __name__ == '__main__':
 
-#     load_MNIST()
+    # load_MNIST()
+    # make_gif('mnist')
